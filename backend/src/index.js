20a632aee417d22b4793cb93fed4d58c656b9631
Mohit Khamari <mohitkhamarim24@gmail.com>
2025-06-16 06:47:55 +0530

    Setting up baackend

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const app = express();
+dotenv.config();
+
+app.get('/' , (req,res) => {
+     res.send("System is running...")
+})
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(process.env.PORT, () => {
+      console.log(`🟢 Server running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB connection error:', err);
+  });
